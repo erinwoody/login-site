9c7a15dda138e39bbfa72232101c5d3d9d5f4222
Erin Woody <erinelizabethwoody@gmail.com>
2017-08-24 14:42:59 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const session = require("express-session");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+const sessionConfig = require("./sessionConfig");
+const app = express();
+const port = process.env.PORT || 8000;
+
+//TEMPLATING ENGINE
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
+
+//MIDDLEWARE
+app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session(sessionConfig));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup"); 
+});
+
+app.post("/signup", (req, res) => {
+    let newUser = req.body;
+    users.push(newUser);
+    res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+    
+})
+
+
+
+
+app.listen(port, () => {
+    console.log("Running on port ${port}!");
+}); 
---
+++ sessionConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+    secret: "a secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
+};
