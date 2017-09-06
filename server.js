6e4b6931dc53d45ff6c849f80e6aacfe7cad9555
Erin Woody <erinelizabethwoody@gmail.com>
2017-09-06 14:17:34 -0500

    added profile and login routes and css

--- server.js
+++ server.js
@@ -5,44 +5,46 @@ const logger = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
 const sessionConfig = require("./sessionConfig");
-const users = require("./data");
+const users = require("./data.js")
 const checkAuth = require("./middlewares/checkAuth");
 const app = express();
 const port = process.env.PORT || 8000;
 
-//TEMPLATING ENGINE
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
 app.set("view engine", "mustache");
 
-
-//MIDDLEWARE
 app.use(express.static(path.join(__dirname, "./public")));
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionConfig));
 
+
 app.get("/", (req, res) => {
-    console.log(req.session);
-    res.render("home");
+   if (req.session.user) {
+        return res.render("home", { user: req.session.user });
+    } else {
+        return res.render("home");
+    } 
 });
+
 
 app.get("/signup", (req, res) => {
     res.render("signup"); 
 });
 
+
 app.post("/signup", (req, res) => {
     let newUser = req.body;
-
-    console.log("newUser: ", newUser);
     users.push(newUser);
-    console.log("users: ", users);
     res.redirect("/login");
 });
+
 
 app.get("/login", (req, res) => {
     res.render("login");
 });
+
 
 app.post("/login", (req, res) => {
     let reqUsername = req.body.username;
@@ -53,17 +55,24 @@ app.post("/login", (req, res) => {
         return res.render("login", { errors: ["User not found"] });
     }
 
-    if (foundUser.password === reqPassword) {
+    if (foundUser.password !== reqPassword) {
+        return res.render("login", { errors: ["Password does not match"] });
+    }
         delete foundUser.password;
         req.session.user = foundUser;
         res.redirect("/");
-    }   else {
-        return res.render("login", { errors: ["Password does not match"] });    
-    }
+    
 });
+
 
 app.get("/profile", checkAuth, (req, res) => {
     res.render("profile", { user: req.session.user });
+});
+
+
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.redirect('/');
 });
 
 
